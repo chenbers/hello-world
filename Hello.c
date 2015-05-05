57afe752e1ee074f812714515d3a900c2dbf7dcf
chenbers.chen <chenbers@gmail.com>
2015-05-05 09:50:37 -0600

    chenbers second commit

--- Hello.c
+++ Hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
+   int x=12;
+   int y;
+   y= x & -x;
+   printf("y=%d\n",y);
    printf ("Hello from your first program!\n");
+   system("echo `date` >> /tmp/mylog");
+   printf ("Test for Jenkins\n" );
    return 0;
 }
