b0f23f7d00ef08703796f60d5b2b1c99d117ba60
chenbers <chenbers@gmail.com>
2015-12-23 23:39:55 -0800

    test for jenkins

--- Hello.c
+++ Hello.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-   int x=12;
-   int y;
-   y= x & -x;
+   //int x=12;
+   //int y;
+   //y= x & -x;
    printf("y=%d\n",y);
    printf ("Hello from your first program!\n");
+   printf("2\n");
    system("echo `date` >> /tmp/mylog");
+   system("echo  Hi... >> /tmp/mylog");
    printf ("Test for Jenkins\n" );
    return 0;
 }
