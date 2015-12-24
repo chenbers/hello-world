5cc919cea3b0657fe010b6d43f6a6fad103de36d
chenbers <chenbers@gmail.com>
2015-12-24 02:21:05 -0800

    test again

--- Hello.c
+++ Hello.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-   //int x=12;
-   //int y;
-   //y= x & -x;
-   //printf("y=%d\n",y);
-   printf ("Hello from your first program!\n");
+   int x=12;
+   int y;
+   y= x & -x;
+   printf("y=%d\n",y);
+   printf ("Hi..Hello from your first program!\n");
    printf("2\n");
    system("echo `date` >> /tmp/mylog");
    system("echo  Hi... >> /tmp/mylog");
